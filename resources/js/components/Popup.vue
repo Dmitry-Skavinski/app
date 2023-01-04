c8cb798561cddb6e2fb0518812c57b0f48d1bb2f
Dmitry-Skavinski <d.skavinskiy@mail.ru>
2023-01-04 15:38:15 +0300

    FE-8 created Popup

---
+++ resources/js/components/Popup.vue
@@ -0,0 +1,32 @@
+<script setup>
+const { isOpen, close } = defineProps([ 'isOpen', 'close' ]);
+</script>
+
+<template>
+    <Teleport to="body" v-if="isOpen">
+        <div class="popup_background" @click="close"/>
+        <div class="popup_content">
+            <slot></slot>
+        </div>
+    </Teleport>
+</template>
+
+<style lang="scss">
+.popup {
+    &_background {
+        height: 100vh;
+        width: 100vw;
+        background: rgba($color: white, $alpha: 0.3);
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+
+    &_content {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+}
+</style>
