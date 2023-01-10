fdae66b4d646100154dbfc5d98caa021da636036
Dmitry-Skavinski <d.skavinskiy@mail.ru>
2023-01-10 14:54:18 +0300

    OTH-1 updated app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
-import {createApp} from 'vue'
+import axios from 'axios';
+import { createApp } from 'vue';
 
-import App from './components/App.vue'
+import App from './components/App.vue';
 import { router } from './router';
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Accept'] = 'application/json';
 
 const app = createApp(App);
 app.use(router);
