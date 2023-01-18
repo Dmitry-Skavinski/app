19584db366b72a09092a9f166ad5e4953921621b
Dmitry-Skavinski <d.skavinskiy@mail.ru>
2023-01-18 21:18:53 +0300

    Created model, routes and Controller for nonograms

---
+++ app/Http/Controllers/NonogramController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nonogram;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class NonogramController extends Controller
+{
+    public function list()
+    {
+        $nonograms = Nonogram::paginate(20, ['*'], 'p');
+        return response()->json($nonograms);
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'min:3|required',
+            'height' => 'numeric|min:5|max:100|required',
+            'width' => 'numeric|min:5|max:100|required',
+        ]);
+
+        if (!Nonogram::isValid($request->nonogram)) {
+            return response('8====D', 400);
+        }
+
+        $validated['task'] = Nonogram::createTask($request->nonogram, $request->height, $request->width);
+        $validated['result'] = $request->nonogram;
+        $nonogram = Nonogram::create($validated);
+        $nonogram->author()->associate(Auth::user());
+        $nonogram->save();
+        return response()->json($nonogram);
+    }
+    public function show($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\NonogramController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
+
+Route::prefix('nonogram')->group(function() {
+    Route::middleware('auth:sanctum')->post('create', [NonogramController::class, 'create']);
+    Route::get('/list', [NonogramController::class, 'list']);
+    Route::get('/{id}', [NonogramController::class, 'show']);
+});
